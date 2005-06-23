4e812eee23444e5d7c9be18acdfb0ff12db0abe9
Roel Canicula <roelmc@info.com.ph>
2005-06-23 17:47:33 +0000

    Added application link

--- www/admin/applications.tcl
+++ www/admin/applications.tcl
@@ -33,18 +33,20 @@ template::list::create \
 	    label "Application"
 	    display_template {
 		<if @applications.asm_url@ not nil>
-		<a href="@applications.asm_url;noquote@" class="button" target="_blank">Application</a>
+		<a href="@applications.asm_url;noquote@" class="button" target="_blank">View</a>
 		</if>
 		<else>
 		N/A
 		</else>
 	    }
+	    html { align center }
 	}
 	actions {
 	    label ""
 	    display_template {
 		<a href="@applications.approve_url;noquote@" class="button">Approve</a>
 	    }
+	    html { align center }
 	}
     }
 
@@ -58,18 +60,26 @@ db_multirow -extend { approve_url asm_url } applications applications {
 
     # Get associated assessment
     if { [db_0or1row assessment {
-	select a.session_id
+	select ss.session_id
 
 	from dotlrn_ecommerce_section s,
-	dotlrn_catalogi c,
-	cr_items i,
-	as_sessions a
+	(select c.*
+	 from dotlrn_catalogi c,
+	 cr_items i
+	 where c.course_id = i.live_revision) c,
+	(select a.*
+	 from as_assessmentsi a,
+	 cr_items i
+	 where a.assessment_id = i.latest_revision) a,
+	as_sessions ss
 
-	where s.course_id = c.item_id
-	and c.course_id = i.live_revision
-	and c.assessment_id = a.assessment_id
-	and s.community_id = :community_id
-	and a.subject_id = :user_id
+	where s.community_id = :community_id
+	and s.course_id = c.item_id
+	and c.assessment_id = a.item_id
+	and a.assessment_id = ss.assessment_id
+	and ss.subject_id = :user_id
+
+	order by creation_datetime desc
 
 	limit 1
     }] } {
