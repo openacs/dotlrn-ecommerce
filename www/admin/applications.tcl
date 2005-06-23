51aa9a4265c7755813513bb7ca3fd214611f48a5
Roel Canicula <roelmc@info.com.ph>
2005-06-23 16:22:24 +0000

    Added application link

--- www/admin/applications.tcl
+++ www/admin/applications.tcl
@@ -32,8 +32,12 @@ template::list::create \
 	assessment_result {
 	    label "Application"
 	    display_template {
-		&lt;link to asessment result&gt;
-		<br />Open in new window
+		<if @applications.asm_url@ not nil>
+		<a href="@applications.asm_url;noquote@" class="button" target="_blank">Application</a>
+		</if>
+		<else>
+		N/A
+		</else>
 	    }
 	}
 	actions {
@@ -44,11 +48,31 @@ template::list::create \
 	}
     }
 
-db_multirow -extend { approve_url } applications applications {
+db_multirow -extend { approve_url asm_url } applications applications {
     select pretty_name as community_name, person__name(user_id) as person_name, member_state, c.community_id, user_id
     from dotlrn_member_rels_full r, dotlrn_communities_all c
     where r.community_id = c.community_id
     and member_state = 'needs approval'
 } {
     set approve_url [export_vars -base application-approve { community_id user_id }]
+
+    # Get associated assessment
+    if { [db_0or1row assessment {
+	select a.session_id
+
+	from dotlrn_ecommerce_section s,
+	dotlrn_catalogi c,
+	cr_items i,
+	as_sessions a
+
+	where s.course_id = c.item_id
+	and c.course_id = i.live_revision
+	and c.assessment_id = a.assessment_id
+	and s.community_id = :community_id
+	and a.subject_id = :user_id
+
+	limit 1
+    }] } {
+	set asm_url [export_vars -base /assessment/asm-admin/results-session { session_id }]
+    }
 }
