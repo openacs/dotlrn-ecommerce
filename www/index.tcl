0dd9555d56edb55da3b229a39e29ffa4ef6edd6f
Roel Canicula <roelmc@info.com.ph>
2005-06-28 22:03:12 +0000

    Check if user is already a dotlrn user

--- www/index.tcl
+++ www/index.tcl
@@ -10,6 +10,12 @@ set cc_package_id [apm_package_id_from_key "dotlrn-catalog"]
 
 set user_id [ad_conn user_id]
 
+if { $user_id } {
+    if { ! [dotlrn::user_p -user_id $user_id] } {
+	dotlrn::user_add -user_id $user_id
+    }
+}
+
 if {[permission::permission_p -party_id $user_id -object_id $cc_package_id -privilege "create"]} {
     set create_p 1
 } else {
