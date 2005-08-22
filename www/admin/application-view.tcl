8cffd9244fc516b60a20b36a7729e7fadd329a29
Tracy Adams <teadams@alum.mit.edu>
2005-08-22 22:58:08 +0000

    adding

---
+++ www/admin/application-view.tcl
@@ -0,0 +1,16 @@
+# packages/dotlrn-ecommerce/www/admin/application-reject.tcl
+
+ad_page_contract {
+        
+    @author Tracy Adams (teadams@alum.mit.edu)
+    @creation-date 2005-08-022
+    @arch-tag: 
+    @cvs-id $Id$
+} {
+    session_id:integer,notnull
+
+} -properties {
+} -validate {
+} -errors {
+}
+
