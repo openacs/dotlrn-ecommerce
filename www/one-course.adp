518f7bb6d79178c05411d376856f055bae3f889a
Dave Bauer <dave@thedesignexperience.org>
2007-01-24 18:32:36 +0000

    Make a page that shows info for just one course.

--- www/one-course.adp
+++ www/one-course.adp
@@ -1,1 +1,2 @@
-<h1>@course_key@</h1>
+<master>
+<include src="/packages/dotlrn-ecommerce/lib/tree-chunk" course_id="@course_id@" section_id="@section_id@" show_filters_p="0">
--- www/one-course.tcl
+++ www/one-course.tcl
@@ -5,14 +5,15 @@ ad_page_contract {
     @author Dave Bauer (dave@thedesignexperience.org)
     @creation-date 2005-06-02
 } {
+    course_id:integer,optional
+    section_id:integer,optional
     cal_item_id:integer,optional
 }
 
 if {[info exists cal_item_id] && ![string equal "" $cal_item_id]} {
 set course_key [db_string get_course_key "select d1.course_key from dotlrn_catalog d1,cr_items ci, (select distinct des.course_id from cal_items c,dotlrn_ecommerce_section des, dotlrn_communities dc, portal_element_map pem, portal_pages pp, portal_element_parameters pep where pep.key='calendar_id' and pep.value=c.on_which_calendar and pem.element_id=pep.element_id and pem.page_id=pp.page_id and pp.portal_id=dc.portal_id and c.cal_item_id=:cal_item_id and dc.community_id=des.community_id) d2 where d1.course_id=ci.live_revision and ci.item_id=d2.course_id" -default ""]
-    ad_returnredirect "./\#${course_key}" 
-    ad_script_abort
+
 }
 
-# TODO make a course details page
+
 
