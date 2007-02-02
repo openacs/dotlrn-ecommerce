0f4395ac9853b5013691c3d3a6af16364d8493d5
Dave Bauer <dave@thedesignexperience.org>
2007-02-02 16:39:59 +0000

    Get course_id from cal_item_id

--- www/one-course.tcl
+++ www/one-course.tcl
@@ -16,7 +16,7 @@ set admin_p [permission::permission_p \
 		 -party_id $user_id \
 		 -privilege admin]
 if {[info exists cal_item_id] && ![string equal "" $cal_item_id]} {
-set course_key [db_string get_course_key "select d1.course_key from dotlrn_catalog d1,cr_items ci, (select distinct des.course_id from cal_items c,dotlrn_ecommerce_section des, dotlrn_communities dc, portal_element_map pem, portal_pages pp, portal_element_parameters pep where pep.key='calendar_id' and pep.value=c.on_which_calendar and pem.element_id=pep.element_id and pem.page_id=pp.page_id and pp.portal_id=dc.portal_id and c.cal_item_id=:cal_item_id and dc.community_id=des.community_id) d2 where d1.course_id=ci.live_revision and ci.item_id=d2.course_id" -default ""]
+set course_id [db_string get_course_id "select ci.item_id from dotlrn_catalog d1,cr_items ci, (select distinct des.course_id from cal_items c,dotlrn_ecommerce_section des, dotlrn_communities dc, portal_element_map pem, portal_pages pp, portal_element_parameters pep where pep.key='calendar_id' and pep.value=c.on_which_calendar and pem.element_id=pep.element_id and pem.page_id=pp.page_id and pp.portal_id=dc.portal_id and c.cal_item_id=:cal_item_id and dc.community_id=des.community_id) d2 where d1.course_id=ci.live_revision and ci.item_id=d2.course_id" -default ""]
 
 }
 set course_return_url [ad_conn url]
