12554545586b92058c3d33ee79bf618ec8bfe178
Hector Romojaro <hector.romojaro@gmail.com>
2018-03-23 22:56:43 +0000

    Fix typos

--- www/ecommerce/shopping-cart.tcl
+++ www/ecommerce/shopping-cart.tcl
@@ -336,7 +336,7 @@ if { !$shipping_gateway_in_use } {
     if { [string equal $no_shipping_options "f" ] } {
 
         # standard shipping is total_reg_shipping_price
-        set shipping_options "Shipping is addtional:"
+        set shipping_options "Shipping is additional:"
         if { $offer_express_shipping_p } {
             # express shipping is total_exp_shipping_price
             set shipping_options "Shipping is additional, choices are:"
